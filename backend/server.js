1b5894fbd95f35911c8796199f8a4e8246fee642
GyeongWonDo <appchemist@daum.net>
2022-10-21 16:20:18 +0900

    first commit

---
+++ backend/server.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const db = require('./db');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.get('/api/values', function (req, res) {
+    db.pool.query(`SELECT * FROM lists;`,
+        (err, results, fileds) => {
+            if (err)
+                return res.status(500).send(err)
+            else
+                return res.json(results)
+        });
+})
+
+app.post('/api/value', function (req, res, next) {
+    console.log(req.body);
+    console.log(req.body.value);
+
+    db.pool.query(`INSERT INTO lists (value) VALUES("${req.body.value}")`,
+        (err, results, fileds) => {
+            if (err)
+                return res.status(500).send(err)
+            else
+                return res.json({success: true, value: req.body.value})
+        })
+})
+
+
+app.listen(5000, () => {
+    console.log("애플리케이션 시작 : 5000")
+})
